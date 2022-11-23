193b632774e19634a62f9b30be49d61208a1699f
Germain Garcia Guerrero <421123687@pcpuma.acatlan.unam.mx>
2022-11-23 14:20:58 -0600

    Creacion de modelo y carga producto

---
+++ modelo.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE tienda;
+USE tienda;
+
+CREATE TABLE producto (
+  sku VARCHAR(20) PRIMARY KEY,
+  descripcion VARCHAR(255) NOT NULL,
+  precio1 DOUBLE NOT NULL,
+  precio2 DOUBLE,
+  costo DOUBLE,
+  rodada INT,
+  existencia INT,
+  linea VARCHAR(255) NOT NULL,
+  marca VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE vendedor (
+  vendedor_id INT PRIMARY KEY,
+  nombre VARCHAR(255) NOT NULL,
+  celular VARCHAR(20),
+  sueldo DOUBLE
+);
+
+CREATE TABLE cliente (
+  cliente_id INT PRIMARY KEY,
+  nombre VARCHAR(255) NOT NULL,
+  celular VARCHAR(20)
+);
+
+CREATE TABLE venta (
+  pdv VARCHAR(20) NOT NULL,
+  fecha DATE NOT NULL,
+  folio INT,
+  producto VARCHAR(20),
+  cliente INT,
+  vendedor INT,
+  cantidad INT,
+  tipoPago VARCHAR(20),
+  importe DOUBLE,
+  FOREIGN KEY (producto) REFERENCES producto(sku),
+  FOREIGN KEY (cliente) REFERENCES cliente(cliente_id),
+  FOREIGN KEY (vendedor) REFERENCES vendedor(vendedor_id)
+);
---
+++ tabla_producto.R
@@ -0,0 +1,57 @@
+rm(list = ls())
+pacman::p_load(tidyverse, DBI, RMySQL)
+
+marca <- c("Canodale", "CUBE", "Brompton", "Specia ilized", "Merida", "Giant", "Scott", "SantaCruz", NA)
+linea <- c("Montaña", "Ruta", "Urbana", "Plegable", "Paseo", "Hibrida", "BMX", "Electrica", NA)
+rodada <- c(12, 14, 16, 18, 20, 22, 26, NA)
+
+bicicletas <- crossing(
+    rodada = rodada, linea = linea,
+    marca = marca
+)
+
+bicicletas$precio1 <- 1200 + runif(nrow(bicicletas), -500, 2000)
+bicicletas$precio2 <- bicicletas$precio1 + runif(nrow(bicicletas), -200, 100)
+bicicletas$costo <- bicicletas$precio1 - runif(nrow(bicicletas), 300, 800)
+bicicletas$descripcion <- paste("Bicicleta", bicicletas$linea, "marca", bicicletas$marca)
+
+
+accesorios <- crossing(
+    linea = c("Proteccion", "Ropa", "Herramienta"),
+    marca = marca
+)
+
+accesorios$precio1 <- 300 + runif(nrow(accesorios), -100, 800)
+accesorios$precio2 <- accesorios$precio1 + runif(nrow(accesorios), -80, 100)
+accesorios$costo <- accesorios$precio1 - runif(nrow(accesorios), 100, 200)
+accesorios$descripcion <- paste("Accesorio para bicicleta: ", accesorios$linea, "marca", accesorios$marca)
+accesorios$rodada <- 0
+
+
+productos <- rbind(bicicletas, accesorios)
+productos <- productos %>%
+    mutate(
+        sku = paste0(
+            toupper(substr(marca, 1, 2)),
+            toupper(substr(linea, 1, 2)),
+            rodada
+        ),
+        existencia = as.integer(
+            runif(nrow(productos), 2, 20)
+        )
+    )
+
+con <- dbConnect(RMySQL::MySQL(),
+    user = "master", password = "1234",
+    dbname = "tienda", host = "localhost"
+)
+
+dbListTables(con)
+
+tablasql <- dbReadTable(con, "producto")
+cols <- names(tablasql)
+
+nueva <- productos %>% select(cols)
+
+dbWriteTable(con, "producto", nueva, append = TRUE, row.names = FALSE)
+dbDisconnect(con)
